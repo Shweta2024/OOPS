de6da7a2bbe01c9b426452169361b21e974ac7bd
Shweta Bhagat <75883328+Shweta2024@users.noreply.github.com>
2022-01-09 20:39:38 +0530

    Update operator_overloading.cpp

--- POLYMORPHISM/operator_overloading.cpp
+++ POLYMORPHISM/operator_overloading.cpp
@@ -9,17 +9,3 @@ Other example classes where arithmetic operators may be overloaded are : Complex
 NOTE :- Operator functions are same as normal functions. The only differences are, name of an operator function is always operator keyword followed by symbol of operator and operator functions are called when the corresponding operator is used. 
 */
 
-#include<bits/stdc++.h>
-using namespace std;
-
-
-class operator_overloading_example{
-    public:
-    
-}
-
-
-int main()
-{
-
-}
